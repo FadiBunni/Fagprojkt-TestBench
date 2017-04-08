d1dd4c80b45ee694b719aaa08ba30d376917e360
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 21:47:16 +0200

    nyeste!

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 
 var mySerial = new SerialPort('/dev/ttyMCC', {
-	baudrate: 9600
-	parser: serialport.parser.readline("\n");
+	baudrate: 9600,
+	parser: serialport.parsers.readline("\n")
 });
 
 mySerial.on('open', function(){
