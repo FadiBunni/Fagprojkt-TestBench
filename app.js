80120c18bb31ed8e8a629c481aa2078b2f9a0a60
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 21:15:11 +0200

    nyeste!

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-var five = require("johnny-five");
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
 
-// Johnny-Five will try its hardest to detect the port for you,
-// however you may also explicitly specify the port by passing
-// it as an optional property to the Board constructor:
-var board = new five.Board({
-  port: "/dev/ttyMCC"
+var mySerial = new SerialPort('/dev/ttyMCC', {
+	baudrate: 9600;
+	parser: serialport.parser.readline("\n");
 });
 
-// The board's pins will not be accessible until
-// the board has reported that it is ready
-board.on("ready", function() {
-  this.pinMode(13, this.MODES.OUTPUT);
+mySerial.on('open', function(){
+	console.log("Port open");
+});
 
-  this.loop(500, function() {
-    // Whatever the last value was, write the opposite
-    this.digitalWrite(13, this.pins[13].value ? 0 : 1);
-  });
+mySerial.on('data', function(data){
+	console.log(data);
 });
