20160d2d426a6cc09cd926295e2e549497aeb2c4
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-08 17:02:16 +0200

    1

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var five = require("johnny-five");
-var board = new five.Board();
+
+// Johnny-Five will try its hardest to detect the port for you,
+// however you may also explicitly specify the port by passing
+// it as an optional property to the Board constructor:
+var board = new five.Board({
+  port: "/dev/ttyMCC";
+});
 
 // The board's pins will not be accessible until
 // the board has reported that it is ready
 board.on("ready", function() {
-  console.log("Ready!");
+  this.pinMode(13, this.MODES.OUTPUT);
 
-  var led = new five.Led(13);
-  led.blink(500);
+  this.loop(500, function() {
+    // Whatever the last value was, write the opposite
+    this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+  });
 });
