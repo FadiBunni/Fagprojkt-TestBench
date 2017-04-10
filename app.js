c2c26f3728827592a997747b8db460274435d419
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-11 01:07:38 +0200

    nyeste!

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyMCC');
+var sp = new SerialPort('/dev/ttyMCC');
 
-port.on('open', function() {
-  port.write('main screen turn on', function(err) {
+sp.on('open', function() {
+  sp.write('main screen turn on', function(err) {
     if (err) {
       return console.log('Error on write: ', err.message);
     }
@@ -11,15 +11,14 @@ port.on('open', function() {
 });
 
 // open errors will be emitted as an error event
-port.on('error', function(err) {
+sp.on('error', function(err) {
   console.log('Error: ', err.message);
 });
 
-port.on('data', function (data) {
+sp.on('data', function (data) {
 	if(data){
   		console.log('Data: ' + data);
-  		port.write('Recived the data')
 	}
 });
 
-port.write('heeys')
+port.write('Hello World');
