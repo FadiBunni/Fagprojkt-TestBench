935a23b5b2a4e4a8e9d22d294e12d05cad329961
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-11 01:24:47 +0200

    nyeste!

--- app.js
+++ app.js
@@ -2,18 +2,7 @@ var SerialPort = require('serialport');
 var sp = new SerialPort('/dev/ttyMCC');
 
 sp.on('open', function() {
-  sp.write('main screen turn on', function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-    console.log('message written');
-  });
   sp.write('Hello World');
-});
-
-// open errors will be emitted as an error event
-sp.on('error', function(err) {
-  console.log('Error: ', err.message);
 });
 
 sp.on('data', function (data) {
