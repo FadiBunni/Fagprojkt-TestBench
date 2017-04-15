f21280f62a7c7e307795736a23f3173b3586ce49
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-15 00:33:46 +0000

    nyeste

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
+var net = require('net');
 var SerialPort = require('serialport');
 var sp = new SerialPort('/dev/ttyMCC');
 
-sp.on('open', function() {
-	setInterval(function(){
-		setTimeout(function(){
-			sp.write("0");
-		},1000);
-		console.log("hello!");
-		sp.write("1");
+var socket = new net.Socket();
+socket.connect(3000,'192.168.0.107', function() {
+	console.log("Client: Connected to server");
+	//socket.write('Hello, server! Love, Client.');
+});
 
-	},1000);
+sp.on('open', function() {
+	socket.on('data', function(data) {
+		var message = data;
+   
+   	//console.log("Response from server: %s", data.response);
+   	//socket.write(JSON.stringify({ response: "Hey there server!" }));
+		//socket.end(); // kill client after server's response
+		if(message == "3"){
+			socket.end();
+			console.log("recieved: "+ message + " server shutting down");
+		}
+		sp.write(message);
+		console.log("data:"+ message + " has been sent");
+	});
+});
+
+socket.on('close', function() {
+	console.log('Connection closed');
+	socket.destroy();
 });
 
 sp.on('data', function (data) {
 	if(data){
   		//console.log('Data: ' + data);
 	}
+});
+
+
+
+
+
+
+socket.on('close', function() {
+	console.log('Connection closed');
+	socket.destroy();
 });
