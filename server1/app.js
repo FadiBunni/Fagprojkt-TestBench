0b84e3db7701090f34930d51450790713ae53da2
FadiBunni <fadibunni_guitar@hotmail.com>
2017-05-29 22:49:43 +0200

    client side added
    weeee

--- server1/app.js
+++ server1/app.js
@@ -18,13 +18,22 @@ http.listen(port,function(){
 	console.log('Server started: http://localhost:2000/');
 });
 
+var direction;
 
 io.on('connection', function(socket){
 	console.log('user connected: ', socket.id);
+
+	socket.on("getDirectionData", function(data){
+		//console.log(data);
+
+		direction = data;
+
+	})
+
 });
 
 
-//////////////
+////////////
 
 var server = net.createServer(function(conn) {
 	console.log("Server: Client connected");
@@ -36,24 +45,14 @@ var server = net.createServer(function(conn) {
 		process.exit(0);
 	});
 
-	var tick = 0;
-	while(tick <= 3){
-		conn.write("0");
-		sleep(1000)
-		conn.write("1");
-		sleep(1000)
-		conn.write("2");
-		sleep(1000)
-		if(tick === 3){
-			conn.write("3");
-		}
-		tick += 1;
-		console.log(tick)
-	}
+	setInterval(function(){
+		console.log(direction);
+		conn.write(direction.toString());
+	},45);
+	//console.log("hey");
 });
 
-
 //Enduroam changes the ip, remember to check. 
-server.listen(3000, "10.16.175.201", function(){
+server.listen(3000, "192.168.0.17", function(){
     console.log("Server: Listening");
 });
--- server1/client/js/sketch.js
+++ server1/client/js/sketch.js
@@ -4,23 +4,62 @@ socket.on('connected', 'hey');
 
 var s = function(p) {
 
+  var value = false;
+  var direction = 0;
+
   p.setup = function() {
     //create canvas and center it
     var canvas = p.createCanvas(800, 800);
     centerCanvas(canvas);
-    p.background(0);
-
-    arrow(p,200,200,5);
-
-
-
+    p.background(255);
+    left = 0;
+    right = 0;
   };
 
   p.draw = function() {
-  	
 
-    
+      p.fill(128,128,128);
+      Arrow(340,390,10,1);
+      Arrow(500,390,10,0);
+    if(value === 0){
+      p.fill(128,128,128);
+      Arrow(340,390,10,1);
+      Arrow(500,390,10,0);
+    }else if(value === 1){
+      p.fill(0,204,0);
+      Arrow(340,390,10,1);
+      direction += 1;
+    }else if(value === 2){
+      p.fill(0,204,0);
+      Arrow(500,390,10,0);
+      direction -= 1;
+    }
+    console.log(direction);
+    //console.log(direction)
+    if(direction >=50){
+      direction = 50;
+    }else if(direction <= -50){
+      direction =-50;
+    }
+    socket.emit("getDirectionData",direction);
   };
+
+  p.keyPressed = function(){
+    if(p.keyCode ===  p.LEFT_ARROW){
+      value = 1;
+    }else if(p.keyCode === p.RIGHT_ARROW){
+      value = 2;
+    }
+  };
+
+  p.keyReleased = function(){
+    if(p.keyCode ===  p.LEFT_ARROW || p.keyCode === p.RIGHT_ARROW ){
+      value = 0;
+      // direction = 0;
+    }
+  };
+
+
 };
 
 var myp5 = new p5(s);
@@ -32,16 +71,19 @@ function centerCanvas(canvas) {
   canvas.position(x, y);
 }
 
-function arrow(p,x,y,length){
-  p.push();
-  p.translate(x,y);
-  p.beginShape();
-  p.vertex(0,-length);
-  p.vertex(5*length,-length);
-  p.vertex(5*length,-3*length);
-  p.vertex(9*length,0);
-  p.vertex(5*length,3*length);
-  p.vertex(5*length,length);
-  p.vertex(0,length);
-  p.endShape();
+function Arrow(x,y,length,angle){
+  myp5.push();
+  myp5.translate(x,y);
+  myp5.rotate(myp5.PI/angle);
+  myp5.beginShape();
+  myp5.vertex(0,-length);
+  myp5.vertex(5*length,-length);
+  myp5.vertex(5*length,-3*length);
+  myp5.vertex(9*length,0);
+  myp5.vertex(5*length,3*length);
+  myp5.vertex(5*length,length);
+  myp5.vertex(0,length);
+  myp5.vertex(0,-length);
+  myp5.endShape();
+  myp5.pop();
 }
